5dd3bed7c6f0b92e2eef88ae79e92b6a570151c1
Xiang Li <lixiang020317@gmail.com>
2024-03-01 12:56:07 +0000

    initial project

--- lib/web-api-stack.ts
+++ lib/web-api-stack.ts
@@ -1,16 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
+import { Construct } from 'constructs';
 export class WebApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const WebApiFn = new lambdanode.NodejsFunction(this, "WebApiFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambdas/webApi.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'WebApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
